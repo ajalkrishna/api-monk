1b3ee59fe06af6f06f4898f774d33bc1cf622d81
ajalkrishna <ajalkrishnav@gmail.com>
2023-02-27 18:58:40 +0530

    made the webpage responsive

--- app/components/api-template.js
+++ app/components/api-template.js
@@ -1,9 +1,15 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApiTemplateComponent extends Component {
-    @action gotoApi(url){
-    window.open(url,'_blank')
+  @tracked copiedUrl = '';
 
-    }
+  @action gotoApi(url) {
+    window.open(url, '_blank');
+  }
+  @action copyToClipboard(url) {
+    this.copiedUrl = url;
+    navigator.clipboard.writeText(url);
+  }
 }
--- app/controllers/collections.js
+++ app/controllers/collections.js
@@ -1,14 +1,100 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
 export default class CollectionsController extends Controller {
-  @tracked jsonData = 'test';
+    apiData = [
+        {
+            category: 'products',
+            url: 'https://dummyjson.com/products',
+            source: 'dummyjson.com',
+            endpoint: '/products',
+        },
+        {
+            category: 'products',
+            url: 'https://dummyjson.com/carts',
+            source: 'dummyjson.com',
+            endpoint: '/carts',
+        },
+        {
+            category: 'users',
+            url: 'https://dummyjson.com/users',
+            source: 'dummyjson.com',
+            endpoint: '/users',
+        },
+        {
+            category: 'users',
+            url: 'https://dummyjson.com/posts',
+            source: 'dummyjson.com',
+            endpoint: '/posts',
+        },
+        {
+            category: 'users',
+            url: 'https://dummyjson.com/comments',
+            source: 'dummyjson.com',
+            endpoint: '/comments',
+        },
+        {
+            category: 'users',
+            url: 'https://dummyjson.com/quotes',
+            source: 'dummyjson.com',
+            endpoint: '/quotes',
+        },
+        {
+            category: 'users',
+            url: 'https://dummyjson.com/todos',
+            source: 'dummyjson.com',
+            endpoint: '/todos',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/posts',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/comments',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/comments',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/albums',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/albums',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/photos',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/photos',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/users',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/users',
+        },
+        {
+            category: 'users',
+            url: 'https://jsonplaceholder.typicode.com/todos',
+            source: 'jsonplaceholder.typicode.com',
+            endpoint: '/todos',
+        },
+        {
+            category: 'users',
+            url: 'https://reqres.in/api/users',
+            source: 'reqres.in',
+            endpoint: '/users',
+        },
+    ];
+    @tracked apiToShow = this.apiData;
+    @tracked searchKeyword;
 
-  @action async fetchJsonData(endpoint) {
-    let response = await fetch(endpoint);
-    let data = await response.json();
-    console.log(data);
-    this.jsonData = JSON.stringify(data);
-  }
+    @action searchApi() {
+        console.log(this.searchKeyword);
+        let filtered = this.apiData.filter(api => api.category == this.searchKeyword)
+        this.apiToShow=filtered;
+        console.log(this.apiToShow);
+    }
 }
